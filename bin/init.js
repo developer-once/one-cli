1454bd99ac260abf103daf6ce8a6398606ac9956
mriliujy <mriliujy@gmail.com>
2022-08-30 21:51:58 +0800

    feat: change bin command

--- bin/init.js
+++ bin/init.js
@@ -1,18 +1,21 @@
 #! /usr/bin/env node
 const program = require('commander');
 program.version(require('../package.json').version);
+program
+  .option('init, --init <type>', '当前目录初始化项目')
+  .option('create, --create <type>', '当前目录创建模版')
 program 
-  .command('init')
-  .description('init project ')
-  .action(() => {
-      // ---- 执行 lib 下的文件 ----
-      require("../lib/init")
+  .command('init <type> [vite-react-ts]')
+  .description('init project')
+  .action((source, destination) => {
+    // ---- 执行 lib 下的文件 ----
+    require("../lib/init", source)
   })
-  program
-  .command('create')
-  .description('create pages')
-  .action(() => {
-      // ---- 执行 lib 下的文件 ----
-      require("../lib/create")
+program 
+  .command('create <type> [page]')
+  .description('create page')
+  .action((source, destination) => {
+    // ---- 执行 lib 下的文件 ----
+    require("../lib/create", source)
   })
 program.parse(process.argv)
