0ff620f94379add23e09254c19ac3a3827703384
mriliujy <mriliujy@gmail.com>
2022-09-01 20:49:40 +0800

    feat: add cz commands

--- bin/init.js
+++ bin/init.js
@@ -1,21 +1,38 @@
 #! /usr/bin/env node
 const program = require('commander');
+
 program.version(require('../package.json').version);
+
 program
   .option('init, --init <type>', '当前目录初始化项目')
   .option('create, --create <type>', '当前目录创建模版')
+
+// ---------- init ----------
 program 
-  .command('init <type> [vite-react-ts]')
+  .command('init')
   .description('init project')
   .action((source, destination) => {
     // ---- 执行 lib 下的文件 ----
     require("../lib/init", source)
   })
-program 
-  .command('create <type> [page]')
+
+// ---------- create ----------
+program
+  .command('create')
   .description('create page')
   .action((source, destination) => {
     // ---- 执行 lib 下的文件 ----
     require("../lib/create", source)
   })
+
+// ---------- cz ----------
+program
+  .command('cz')
+  .description('git commitizen')
+  .action((source, destination) => {
+    // ---- 执行 lib 下的文件 ----
+    require("../lib/create", source)
+  })
+
+
 program.parse(process.argv)
