7f291089108358d2bda5e95dd812c9e4fdcbe200
liulu <1021283712@qq.com>
2023-02-16 00:36:57 +0800

    refa: 删除全局的属性

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,23 +1,26 @@
 #! /usr/bin/env node
 import { Command } from 'commander';
-import init from '../commands/init';
+import pkg from '../../package.json';
 import create from '../commands/create';
 import cz from '../commands/cz';
+import init from '../commands/init';
 
 const program = new Command();
-
-program.version(require('../../package.json').version);
-
-program
-  .option('init, --init <type>', '当前目录初始化项目')
-  .option('create, --create <type>', '当前目录创建模版')
-  .option('cz, --cz', '当前目录下载并配置 package.json 文件 commitizen')
+// 这里设置的是全局的 options
+// program
+//   .option('init, --init <type>', '当前目录初始化项目')
+//   .option('create, --create <type>', '当前目录创建模版')
+//   .option('cz, --cz', '当前目录下载并配置 package.json 文件 commitizen');
 
 // ---------- init ----------
+
 program
   .command('init')
+  // --no-git 指定 options 里的变量名字， 前缀--no 会取反
+  .option('-n, --no-git', '跳过 git 初始化')
   .description('init project')
-  .action(() => {
+  .action((option) => {
+    console.log(option);
     init();
   });
 
@@ -37,4 +40,8 @@ program
     cz();
   });
 
-program.parse(process.argv);
+program
+  .name(Object.keys(pkg.bin)[0])
+  .usage('<command> [options]')
+  .version(pkg.version)
+  .parse(process.argv);
