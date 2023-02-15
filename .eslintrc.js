e29943e34ef20b98efdc9fe8bf16e0b23587ab64
liulu <1021283712@qq.com>
2023-02-16 00:38:44 +0800

    style: 修改eslint 和 tsconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
-  extends: [require.resolve('@umijs/fabric/dist/eslint')],
   env: {
     browser: true,
     es2021: true,
   },
   extends: [
     'airbnb-base',
+    require.resolve('@umijs/fabric/dist/eslint'),
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:import/typescript',
@@ -15,9 +15,7 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint'],
   rules: {
     'import/extensions': [
       'error',
